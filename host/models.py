c388625eee1b6bed8e4006aa32312b8d7defac79
Edward <edwardlshfzw@gmail.com>
2019-03-05 21:32:31 +0800

    导入rbac权限组件，增加使用文件，创建host（业务）表

--- host/models.py
+++ host/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from rbac.models import UserInfo as RbacUserInfo
 
-# Create your models here.
+
+class Department(models.Model):
+    """部门表"""
+    title = models.CharField(verbose_name='部门', max_length=32)
+
+    def __str__(self):
+        return self.title
+
+
+class UserInfo(models.Model):
+    """用户表"""
+    user = models.OneToOneField(verbose_name='用户', to=RbacUserInfo, on_delete=models.CASCADE)
+    phone = models.CharField(verbose_name='联系方式', max_length=32)
+    T1 = 1
+    T2 = 2
+    T3 = 3
+    level_choices = (
+        (T1, 'T1'),
+        (T2, 'T2'),
+        (T3, 'T3')
+    )
+    level = models.IntegerField(verbose_name='级别', choices=level_choices)
+    department = models.ForeignKey(verbose_name='部门', to='Department', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
+
+
+class Host(models.Model):
+    """主机表"""
+    hostname = models.CharField(verbose_name='主机名', max_length=32)
+    ip = models.GenericIPAddressField(verbose_name='IP', protocol='both')
+    department = models.ForeignKey(verbose_name='归属部门', to='Department', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hostname
